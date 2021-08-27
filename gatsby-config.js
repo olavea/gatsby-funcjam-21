5043572fb1db52aacfef210fa4aa675274a4eecf
Ola <ola@olavea.com>
2021-08-27 15:47:17 +0200

    fix: added Ruby's 1st TimeShip

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,35 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
-  siteMetadata: {
-    title: "form",
+  flags: {
+    FUNCTIONS: true,
   },
-  plugins: [],
+  siteMetadata: {
+    title: "Ruby's TimeShip",
+    titleTemplate: "%s ·Built to save Ruby's sister, Princess Lizabeth from prison in the Tower of London",
+    description: "Built by Lillian (6 🏴‍☠️👸 ) and by Ola Vea (43 🦇😺 ) with ❤️ and gatsby functions",
+    siteUrl: `https://rubystimeshipstripe.gatsbyjs.io/`,
+    url: `https://rubystimeshipstripe.gatsbyjs.io/`,
+    image: "/images/icon.png", // Get my OlaVea iconPath to your image you placed in the 'static' folder
+    twitter: "@olaholstvea",
+  },
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `rubys-timeship`,
+        short_name: `timeship`,
+        start_url: `/`,
+        background_color: `#fffaf0`,
+        theme_color: `ff1616`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+        cache_busting_mode: 'none',
+      },
+    },
+
+  ],
 }
